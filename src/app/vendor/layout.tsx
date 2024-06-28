52ff4c7e82f2bc70754e7cd394c57cc7368a80b9
hrishikeshmane <hrishi.mane26@gmail.com>
2024-06-28 01:32:35 -0700

    feat: banner to link stripe account

--- src/app/vendor/layout.tsx
+++ src/app/vendor/layout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { redirect } from "next/navigation";
 import MaxWidthWrapper from "~/components/MaxWidthWrapper";
 import { checkRole } from "~/lib/clerk";
@@ -12,6 +13,13 @@ export default function VendorDashboardLayout({
   }
   return (
     <div className="">
+      {/*  TODO: Show this only when stripe not linked  */}
+      <div className="sticky top-16 flex h-12 w-screen items-center justify-center bg-primary text-primary-foreground">
+        You have not linked your Stripe account.
+        <Link href={"/vendor/billing"} className="ml-1 underline">
+          Link your account to start selling on Unshackled!
+        </Link>
+      </div>
       <div className="">
         <MaxWidthWrapper>{children}</MaxWidthWrapper>
       </div>
