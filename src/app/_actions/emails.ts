4a55c458239e1fb327ea831934c2225102cb17ea
hrishikeshmane <hrishi.mane26@gmail.com>
2024-06-28 21:25:33 -0700

    feat: resend email boilerplate

--- src/app/_actions/emails.ts
+++ src/app/_actions/emails.ts
@@ -3,15 +3,103 @@
 import { Resend } from "resend";
 import { EmailTemplate } from "~/components/email-templates/welcome";
 
-export async function sendWelcomeEmail() {
-  const resend = new Resend(process.env.RESEND_API_KEY);
+const FROM_EMAIL = "Acme <onboarding@resend.dev>";
+const RESEND_KEY = process.env.RESEND_KEY;
 
-  const { data } = await resend.emails.send({
-    from: "Acme <onboarding@resend.dev>",
+export async function sendWelcomeEmail() {
+  const resend = new Resend(RESEND_KEY);
+
+  const { data, error } = await resend.emails.send({
+    from: FROM_EMAIL,
     to: ["delivered@resend.dev"],
     subject: "Hello World",
     react: EmailTemplate({ firstName: "John" }) as React.ReactElement,
   });
 
-  console.log(data);
+  if (error) {
+    console.error(error);
+    return error;
+  }
+
+  return data;
+}
+
+////////////////////////////////////////
+// Vendor onboarding emails
+////////////////////////////////////////
+export async function sendVenorApprovalPendingEmail(email: string) {
+  const resend = new Resend(RESEND_KEY);
+  const { data, error } = await resend.emails.send({
+    from: FROM_EMAIL,
+    to: [email],
+    subject: "Hello World",
+    react: EmailTemplate({ firstName: "John" }) as React.ReactElement,
+  });
+
+  if (error) {
+    console.error(error);
+    return error;
+  }
+
+  return data;
+}
+
+export async function sendVenorIsApprovedEmail(email: string) {
+  const resend = new Resend(RESEND_KEY);
+  const { data, error } = await resend.emails.send({
+    from: FROM_EMAIL,
+    to: [email],
+    subject: "Hello World",
+    react: EmailTemplate({ firstName: "John" }) as React.ReactElement,
+  });
+
+  if (error) {
+    console.error(error);
+    return error;
+  }
+
+  return data;
+}
+
+////////////////////////////////////////
+// Vendor product emails
+////////////////////////////////////////
+export async function sendVenorProductApprovalPendingEmail(
+  email: string,
+  productId: string,
+) {
+  const resend = new Resend(RESEND_KEY);
+  const { data, error } = await resend.emails.send({
+    from: FROM_EMAIL,
+    to: [email],
+    subject: "Hello World",
+    react: EmailTemplate({ firstName: "John" }) as React.ReactElement,
+  });
+
+  if (error) {
+    console.error(error);
+    return error;
+  }
+
+  return data;
+}
+
+export async function sendVenorProductIsApprovedEmail(
+  email: string,
+  productId: string,
+) {
+  const resend = new Resend(RESEND_KEY);
+  const { data, error } = await resend.emails.send({
+    from: FROM_EMAIL,
+    to: [email],
+    subject: "Hello World",
+    react: EmailTemplate({ firstName: "John" }) as React.ReactElement,
+  });
+
+  if (error) {
+    console.error(error);
+    return error;
+  }
+
+  return data;
 }
