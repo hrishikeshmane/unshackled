989a2da0c8049e3f6cda728a5e56fea6e457cbb7
hrishikeshmane <hrishi.mane26@gmail.com>
2024-07-21 20:02:39 -0700

    fix: vendor approve workaround

--- src/app/admin/(dashboard)/[storeId]/(routes)/products/page.tsx
+++ src/app/admin/(dashboard)/[storeId]/(routes)/products/page.tsx
@@ -1,54 +1,59 @@
-import { format } from 'date-fns'
-import { ProductClient } from './_components/client';
-import { api } from '~/trpc/server';
-import { type ProductColumn } from './_components/columns';
-import { type ProductWithRelations } from '~/types/globals';
-import { formatPrice } from '~/lib/utils';
+import { format } from "date-fns";
+import { ProductClient } from "./_components/client";
+import { api } from "~/trpc/server";
+import { type ProductColumn } from "./_components/columns";
+import { type ProductWithRelations } from "~/types/globals";
+import { formatPrice } from "~/lib/utils";
 import { clerkClient } from "@clerk/nextjs/server";
 
-const ProductsPage = async ({ 
-    params
-}: { 
-    params: { storeId: string }
-}) => {
-    const products: ProductWithRelations[] = await api.product.getProductsByStoreId({ storeId: params.storeId });
-    
-    const formattedProducts: ProductColumn[] = await Promise.all(products.map(async item => {
-        let creatorName = item.creatorId;
-        try {
-            const user = await clerkClient.users.getUser(item.creatorId);
-            if (user) {
-                creatorName = `${user.firstName} ${user.lastName}`.trim() ?? user.emailAddresses[0] ?? item.creatorId;
-            }
-        } catch (error) {
-            console.error(`Error fetching user for creatorId ${item.creatorId}:`, error);
+const ProductsPage = async ({ params }: { params: { storeId: string } }) => {
+  const products: ProductWithRelations[] =
+    await api.product.getProductsByStoreId({ storeId: params.storeId });
+
+  const formattedProducts: ProductColumn[] = await Promise.all(
+    products.map(async (item) => {
+      let creatorName = item.creatorId;
+      try {
+        const user = await clerkClient.users.getUser(item.creatorId);
+        if (user) {
+          creatorName =
+            `${user.firstName} ${user.lastName}`.trim() ??
+            user.emailAddresses[0] ??
+            item.creatorId;
         }
+      } catch (error) {
+        console.error(
+          `Error fetching user for creatorId ${item.creatorId}:`,
+          error,
+        );
+      }
 
-        return {
-            id: item.id,
-            name: item.name,
-            creator: creatorName,
-            tag: item.tag ? item.tag.name : "N/A",
-            type: item.type ? item.type.name : "N/A",
-            isFeatured: item.isFeatured,
-            isApproved: item.isApproved,
-            isArchived: item.isArchived,
-            price: formatPrice(Number(item.price)),
-            commission: item.commission,
-            commissionType: item.commissionType,
-            estTurnAroundTime: item.estTurnAroundTime,
-            domainRank: item.domainRank,
-            createdAt: format(item.createdAt, "MMMM do, yyyy"),
-        };
-    }));
+      return {
+        id: item.id,
+        name: item.name,
+        creator: item.creatorId,
+        tag: item.tag ? item.tag.name : "N/A",
+        type: item.type ? item.type.name : "N/A",
+        isFeatured: item.isFeatured,
+        isApproved: item.isApproved,
+        isArchived: item.isArchived,
+        price: formatPrice(Number(item.price)),
+        commission: item.commission,
+        commissionType: item.commissionType,
+        estTurnAroundTime: item.estTurnAroundTime,
+        domainRank: item.domainRank,
+        createdAt: format(item.createdAt, "MMMM do, yyyy"),
+      };
+    }),
+  );
 
-    return (
-        <div className="flex-col">
-            <div className="flex-1 p-8 pt-6 space-y-4">
-                <ProductClient data={formattedProducts} />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <ProductClient data={formattedProducts} />
+      </div>
+    </div>
+  );
+};
 
 export default ProductsPage;
