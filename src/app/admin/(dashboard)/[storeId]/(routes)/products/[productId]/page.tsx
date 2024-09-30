81947ef73fde9a8b0597f5d2184bfffc7c9a96b5
Akash Mali <akashbm08@gmail.com>
2024-09-29 23:54:31 -0400

    (fix): approval form merged in product form

--- src/app/admin/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
+++ src/app/admin/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
@@ -1,15 +1,31 @@
 import { ProductForm } from "./_components/product-form";
 import { api } from "~/trpc/server"
 
+type Question = {
+    type: "short" | "long";
+    id: string;
+    createdAt: Date;
+    updatedAt: Date | null;
+    productId: string;
+    vendorId: string;
+    question: string;
+  };
+
 const ProductPage = async ({ params }: { params: { productId: string, storeId: string } }) => {
     const product = await api.product.getProductById({ id: params.productId });
     const tags = await api.tag.getTagsByStoreId({ storeId: params.storeId });
     const types = await api.types.getTypesByStoreId({ storeId: params.storeId });
+    let questions: Question[] | [] = []
+    if (product && product.id) {
+        questions = await api.approvalForms.getQuestions(
+            { productId: product.id, vendorId: product.creatorId }
+          );
+    }
 
     return (
         <div className="flex-col">
             <div className="flex-1 p-8 pt-6 space-y-4">
-                {<ProductForm initialData={product} tags={tags} types={types} />}
+                {<ProductForm initialData={product} tags={tags} types={types} questions={questions} />}
             </div>
         </div>
     )
--- src/app/vendor/products/[storeId]/product/[productId]/page.tsx
+++ src/app/vendor/products/[storeId]/product/[productId]/page.tsx
@@ -1,6 +1,16 @@
 import { ProductForm } from "./_components/product-form";
 import { api } from "~/trpc/server";
 
+type Question = {
+  type: "short" | "long";
+  id: string;
+  createdAt: Date;
+  updatedAt: Date | null;
+  productId: string;
+  vendorId: string;
+  question: string;
+};
+
 const ProductPage = async ({
   params,
 }: {
@@ -10,10 +20,17 @@ const ProductPage = async ({
   const tags = await api.tag.getTagsByStoreId({ storeId: params.storeId });
   const types = await api.types.getTypesByStoreId({ storeId: params.storeId });
 
+  let questions: Question[] | [] = []
+  if (product && product.id) {
+      questions = await api.approvalForms.getQuestions(
+          { productId: product.id, vendorId: product.creatorId }
+        );
+  }
+
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        {<ProductForm initialData={product} tags={tags} types={types} />}
+        {<ProductForm initialData={product} tags={tags} types={types} questions={questions} />}
       </div>
     </div>
   );
